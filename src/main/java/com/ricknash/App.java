ffb0f2429fcf947386d389c931077b761340a78d
Ilya Lyasin <lyasin.i@mail.ru>
2023-05-13 01:13:53 +0300

    Module_1_4

--- src/main/java/com/ricknash/App.java
+++ src/main/java/com/ricknash/App.java
@@ -10,16 +10,13 @@ public class App {
 
         Foo foo = new Foo(cdl2, cdl3);
 
-        Runnable first = () -> foo.first(new MyThread("first"));
-        Runnable second = () -> foo.second(new MyThread("second"));
-        Runnable third = () -> foo.third(new MyThread("third"));
+        Runnable firstTask = () -> foo.first(new MyThread("first"));
+        Runnable secondTask = () -> foo.second(new MyThread("second"));
+        Runnable thirdTask = () -> foo.third(new MyThread("third"));
 
-        Thread a =  new Thread(first);
-        Thread b =  new Thread(second);
-        Thread c =  new Thread(third);
 
-        c.start();
-        a.start();
-        b.start();
+        new Thread(thirdTask).start();
+        new Thread(firstTask).start();
+        new Thread(secondTask).start();
     }
 }
